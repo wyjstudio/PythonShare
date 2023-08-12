5dc89e8a4c27d3ea6774dc7b1bca8b2ebfd734de
yingjie.wang <yingjie.wang@yiducloud.cn>
2023-08-12 21:02:57 +0800

    add tool of convert image to sketch

---
+++ tools/convert_image.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#encoding=utf-8
+
+import cv2
+#reading image
+image = cv2.imread("cutegirl.png")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+inverted_image = 255 - gray_image
+blurred = cv2.GaussianBlur(inverted_image, (21, 21), 0)
+inverted_blurred = 255 - blurred
+pencil_sketch = cv2.divide(gray_image, inverted_blurred, scale=256.0)
+cv2.imshow("Original Image", image)
+cv2.imshow("Pencil Sketch of Dog", pencil_sketch)
+cv2.waitKey(0)
